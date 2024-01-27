09e71d817dc591ea351695ca0a40952eaac4f1fa
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2024-01-27 14:10:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if nsei_data is not None and dji_data is not None:
 
     # Train regression model
     X = merged_data.iloc[:, 1:]  # Features (excluding Date and ^NSEI columns)
-    y = merged_data['Open^NSEI']
+    y = merged_data['^NSEI']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
